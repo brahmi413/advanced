5cc38a8a9a7b884522693b38880921b7785c716e
brahmi413 <brahmiece413@gmail.com>
2015-07-23 14:02:02 +0530

    sdfdsf

--- common/config/main.php
+++ common/config/main.php
@@ -3,7 +3,7 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache\brhamis',
+            'class' => 'yii\caching\FileCache\brhamisnew',
         ],
     ],
 ];
